80760a0b6706c0877c87536eeae4114a8fd648ea
Tsantanny <hei.tsanta.3@gmail.com>
2025-11-19 16:46:35 +0300

    feat: refactor Wallet class to manage cards as an array and remove cardHolder property

--- src/Wallet.php
+++ src/Wallet.php
@@ -5,21 +5,20 @@ namespace App;
 class Wallet
 {
     private string $color;
-    private string $cardHolder;
+    private array $cards = [];
     private float $weight;
     private string $brand;
     private float $money;
-    private array $cards = [];
 
     public function __construct(
         string $color,
-        string $cardHolder,
+        array $cards,
         float $weight,
         string $brand,
         float $money
     ) {
         $this->color = $color;
-        $this->cardHolder = $cardHolder;
+        $this->cards = $cards;
         $this->weight = $weight;
         $this->brand = $brand;
         $this->money = $money;
@@ -37,6 +36,7 @@ class Wallet
         }
 
         $this->money -= $amount;
+
         return $amount;
     }
 
@@ -55,18 +55,23 @@ class Wallet
     }
 
     // Cards management
-    public function addCard(string $card): array
+    public function addCard(string $card): string
     {
-        if (!in_array($card, $this->cards)) {
+        if (!in_array($card, $this->cards, true)) {
             $this->cards[] = $card;
         }
 
-        return $this->getCards();
+        return $card;
     }
 
     public function getCards(): array
     {
         return $this->cards;
+    }
+
+    public function setCards(array $cards): void
+    {
+        $this->cards = $cards;
     }
 
     // Lost status
@@ -84,16 +89,6 @@ class Wallet
     public function setColor(string $color): void
     {
         $this->color = $color;
-    }
-
-    public function getCardHolder(): string
-    {
-        return $this->cardHolder;
-    }
-
-    public function setCardHolder(string $cardHolder): void
-    {
-        $this->cardHolder = $cardHolder;
     }
 
     public function getWeight(): float
